99dee6295975daa5ed8ee1e6b68513422795029d
N.Manojlovic.inf21 <n.manojlovic.inf21@stud.bbbaden.ch>
2023-09-19 15:47:49 +0200

    Ask the user for date, closes #2

--- helper.py
+++ helper.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import datetime
 
 items = []
 
@@ -6,12 +7,24 @@ items = []
 @dataclass
 class Item:
     text: str
+    date: datetime
     isCompleted: bool = False
 
 
-def add(text):
+def oneWeekFromToday():
+    today = datetime.datetime.now()
+    oneWeek = datetime.timedelta(weeks=1)
+    return today + oneWeek 
+
+
+def add(text, date=None):
     text = text.replace('b', 'bbb').replace('B', 'Bbb')
-    items.append(Item(text))
+
+    if date is None:
+        date = oneWeekFromToday()
+    else:
+        date = datetime.datetime.strptime(date, '%Y-%m-%d')
+    items.append(Item(text, date))
 
 
 def get_all():
